4913b458c10406c4e56986295b86bb839f5186ce
Aleksey <aleksey-asapov@mail.ru>
2022-06-13 13:43:58 +0300

    Task_DZ- решение ДЗ

---
+++ Task_DZ/Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 2: Напишите программу, которая на вход
+// принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 ->  max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+// Console.WriteLine("Введите два числа ");
+// int a = int.Parse(Console.ReadLine());
+// int b = int.Parse(Console.ReadLine());
+// if (a > b)
+// {
+//     Console.WriteLine("Первое число большее");
+// }
+// else
+// {
+//     Console.WriteLine("Второе число большее");
+// }
+
+// Задача 4. Напишите программу,
+// которая принимает на вход три числа и
+// выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Введите числa ");
+// int a = int.Parse(Console.ReadLine());
+// int b = int.Parse(Console.ReadLine());
+// int c = int.Parse(Console.ReadLine());
+// if ((b > a) & (b > c))
+// {
+//     Console.Write("Вывод наибольшего числа - ");
+//     Console.WriteLine(+ b);
+// }
+// else 
+//     if ((c > a) & (c > b))
+//     {
+//     Console.Write("Вывод наибольшего числа - ");
+//     Console.WriteLine(+ c);
+//     }
+//     else
+//     {
+//     Console.Write("Вывод наибольшего числа - ");
+//     Console.WriteLine(+ a);
+//     }
+
+// Задача 6. Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным 
+// (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("Введите число ");
+// int A = int.Parse(Console.ReadLine());
+// if (A % 2 == 0)
+// {
+//     Console.WriteLine("Число четное");
+// }
+// else
+// {
+//     Console.WriteLine("Число нечетное");
+// }
+
+// Задача 8. Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4 
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Введите число ");
+// int N = int.Parse(Console.ReadLine());
+// int i = 1;
+// while (i <= N)
+// {
+//     if (i % 2 == 0)
+//     {
+//         Console.Write(i + " ");
+//     }
+
+//     i++;
+// }
+
